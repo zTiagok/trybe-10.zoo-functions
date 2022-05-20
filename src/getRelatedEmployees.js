4aab527ab0175bd4e2d3496c0d1ab31eff876bcd
Tiago <ztiagok@gmail.com>
2022-05-20 18:48:32 -0500

    c4 - Início da resolução do 4º Requisito.

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,27 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  let searchEmployee = [];
+  
+  employees.find((employee) => {
+    if (employee.id === id) {
+     searchEmployee.push(employee);
+    }
+  });
+
+  return searchEmployee.some((search) => {
+    return search.managers[0] !== ""
+  })
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
 }
+
+isManager('9e7d4524-363c-416a-8759-8aa7e50c0992')
+// 9e7d4524-363c-416a-8759-8aa7e50c0992 GERENTE
+// c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1 NÃO GERENTE
 
 module.exports = { isManager, getRelatedEmployees };
