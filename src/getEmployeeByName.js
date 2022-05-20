312798f46de51f7fdb131d726397e89fe849fdf2
Tiago <ztiagok@gmail.com>
2022-05-20 18:18:49 -0500

    c3 - Resolução do 3º Requisito.

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,10 +1,24 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
   // seu código aqui
   if (!employeeName) {
     return {};
   }
+
+  return employees.find((employee) => {
+      if (employee.firstName === employeeName) {
+        return employee.firstName;
+      } else if (employee.lastName === employeeName) {
+        return employee.lastName;
+      }
+    }
+  )
 }
+
+getEmployeeByName('Wishart')
+
 
 module.exports = getEmployeeByName;
