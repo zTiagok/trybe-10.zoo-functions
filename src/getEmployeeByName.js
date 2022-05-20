325c1532b05fac6e5cca48727b0fe8d2c8408b4a
Tiago <ztiagok@gmail.com>
2022-05-20 17:03:08 -0500

    c1 - Resolução do 1º Requisito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,9 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
 }
 
 module.exports = getEmployeeByName;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+const { species } = data;
+
+const actual = ['baa6e93a-f295-44e7-8f70-2bcdc6f6948d', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'];
+
+const getSpeciesByIds = (...idToSearch) => {
+  const result = [];
+
+  species.forEach((element) => idToSearch.forEach((id) => {
+    if (element.id === id) {
+      result.push(element);
+    }
+  }));
+
+  return result;
+};
+
+console.log(getSpeciesByIds(...actual));
 
 module.exports = getSpeciesByIds;
