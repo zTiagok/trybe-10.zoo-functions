ef4065370b041111b784e22e8e4738563caf567c
Tiago <ztiagok@gmail.com>
2022-05-23 15:32:43 -0500

    c6 - Resolução do 5º Exercício Resolvido.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,41 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  let animalsCounter = {}
+  let sexCounter = 0;
+  let animalsIndex = ['lions', 'otters', 'elephants', 'tigers', 'snakes', 'frogs', 'bears', 'penguins', 'giraffes']
+
+  if (!animal) {
+    species.forEach((specie) => {
+      animalsIndex.forEach((index) => {
+        if (index === specie.name) {
+          animalsCounter[index] = specie.residents.length
+        }
+      })
+    })
+  } else if (animal.specie && !animal.sex) {
+    species.forEach((specie) => {
+      if (specie.name === animal.specie) {
+        animalsCounter = specie.residents.length
+      }
+    })
+  } else if (animal.specie && animal.sex) {
+    species.forEach((specie) => {
+      if (specie.name === animal.specie) {
+        specie.residents.filter((resident) => {
+          if (resident.sex === animal.sex) {
+            sexCounter += 1
+          }
+          animalsCounter = sexCounter;
+        })
+      }
+    })
+  }
+
+  return animalsCounter;
 }
 
+console.log(countAnimals({ specie: 'bears', sex: 'male' }))
 module.exports = countAnimals;
