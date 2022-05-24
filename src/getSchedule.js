d4df36bf61421258bbad738a913191bf31dc4014
Tiago <ztiagok@gmail.com>
2022-05-23 19:01:14 -0500

    c9 - Início da resolução do próximo problema.

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
+const { species, hours } = data;
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  generalSchedule = {}
+  if (scheduleTarget) {
+    return species.find((specie) => {
+      return specie.name === scheduleTarget
+    }).availability
+  } else {
+    generalSchedule.Tuesday = {officeHour: `Open from ${hours.Tuesday.open} until ${hours.Tuesday.close}`,
+exbibition: species.filter((specie) => {
+  return specie.availability.find((day) => {
+    return day.includes('Tuesday')
+  })
+ })}
+
+  }
+
+  console.log(generalSchedule)
 }
 
+console.log(getSchedule())
 module.exports = getSchedule;
