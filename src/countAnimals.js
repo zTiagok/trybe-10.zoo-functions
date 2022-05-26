578b04dd4685da978c0d1fc0d4ccf6dbf4225ae2
Tiago <ztiagok@gmail.com>
2022-05-26 18:54:56 -0500

    c16 - Correção do lint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -39,6 +39,7 @@ const retrieveSpecieAndSex = (name) => {
         if (resident.sex === name.sex) {
           sexCounter += 1;
         }
+        return null;
       });
     }
   });
@@ -57,39 +58,5 @@ function countAnimals(animal) {
     return retrieveSpecieAndSex(animal);
   }
 }
-// function countAnimals(animal) {
-//   let animalsCounter = {};
-//   let sexCounter = 0;
-//   const animalsIndex = ['lions', 'otters', 'elephants', 'tigers', 'snakes', 'frogs', 'bears', 'penguins', 'giraffes'];
-
-//   if (!animal) {
-//     species.forEach((specie) => {
-//       animalsIndex.forEach((index) => {
-//         if (index === specie.name) {
-//           animalsCounter[index] = specie.residents.length;
-//         }
-//       });
-//     });
-//   } else if (animal.specie && !animal.sex) {
-//     species.forEach((specie) => {
-//       if (specie.name === animal.specie) {
-//         animalsCounter = specie.residents.length;
-//       }
-//     });
-//   } else if (animal.specie && animal.sex) {
-//     species.forEach((specie) => {
-//       if (specie.name === animal.specie) {
-//         specie.residents.filter((resident) => {
-//           if (resident.sex === animal.sex) {
-//             sexCounter += 1;
-//           }
-//           animalsCounter = sexCounter;
-//         });
-//       }
-//     });
-//   }
-
-//   return animalsCounter;
-// }
 
 module.exports = countAnimals;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -2,9 +2,27 @@ const data = require('../data/zoo_data');
 
 // const { species, hours } = data;
 
-function getSchedule(scheduleTarget) {
+// const targetExhibition = (target) => species.find((specie) => {
+//   if (specie.name === target) {
+//     return specie;
+//   }
+//   return null;
+// }).availability;
 
+// const allExhibition = () => {
+//   const exhibition = {
+//     Tuesday: {
+//       officeHour: `Open from ${hours.Thursday.open}am until ${hours.Tuesday.close}pm`;
+//     },
+//   };
+// };
+
+// MAIN ------------------------------------------------------
+function getSchedule(scheduleTarget) {
+  // if (scheduleTarget) {
+  //   return targetExhibition(scheduleTarget);
+  // }
 }
 
-console.log(getSchedule('lions'));
+// console.log(getSchedule('lions'));
 module.exports = getSchedule;
