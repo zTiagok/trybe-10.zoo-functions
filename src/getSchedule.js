0e87dd5edf07b0a021e62a97538587620caf3a4f
Tiago <ztiagok@gmail.com>
2022-05-27 11:25:39 -0500

    c17 - Início de mais um requisito

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,28 +1,55 @@
 const data = require('../data/zoo_data');
 
-// const { species, hours } = data;
+const { species, hours } = data;
 
-// const targetExhibition = (target) => species.find((specie) => {
-//   if (specie.name === target) {
-//     return specie;
-//   }
-//   return null;
-// }).availability;
+const targetExhibition = (target) => {
+  return species.find((specie) => {
+    return specie.name === target;
+  }).availability;
+};
 
-// const allExhibition = () => {
-//   const exhibition = {
-//     Tuesday: {
-//       officeHour: `Open from ${hours.Thursday.open}am until ${hours.Tuesday.close}pm`;
-//     },
-//   };
-// };
+const retrieveAnimals = (day) => {
+  const animalArray = [];
+
+  species.forEach((specie) => {
+    specie.availability.filter((available) => {
+      if (available === day) {
+        animalArray.push(specie.name);
+      }
+    });
+  });
+
+  return animalArray;
+};
+
+const retrieveDaysHours = () => {
+  const weekDays = Object.keys(hours);
+  const filteredWeek = {};
+
+  weekDays.map((day, index) => {
+    if (index === 6) {
+      filteredWeek[day] = { officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!' };
+    }
+    filteredWeek[day] = { officeHour: `Open from ${hours[day]
+      .open}am until ${hours[day].close}pm`,
+    exhibition: retrieveAnimals(day) };
+  });
+
+  return filteredWeek;
+};
 
 // MAIN ------------------------------------------------------
 function getSchedule(scheduleTarget) {
-  // if (scheduleTarget) {
-  //   return targetExhibition(scheduleTarget);
-  // }
+  if (scheduleTarget) {
+    return targetExhibition(scheduleTarget);
+  } if (scheduleTarget === undefined) {
+    return retrieveDaysHours();
+  }
+
 }
+
+console.log(retrieveDaysHours());
 
 // console.log(getSchedule('lions'));
 module.exports = getSchedule;
